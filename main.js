746791da33c95a09731382184cbc90339fd0046f
Aamir <aamirnorth@gmail.com>
2023-03-26 15:53:54 +0400

    smooth scroll added

--- main.js
+++ main.js
@@ -3,4 +3,101 @@ import './index.js'
 import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
- Alpine.start()
+Alpine.start()
+
+
+
+const simpleParallax = (elem, modifier) => {
+    let paras = document.getElementsByClassName(elem);
+    for (let i = 0; i < paras.length; i++) {
+      paras[i].setAttribute(
+        "style",
+        "background-repeat: no-repeat; background-attachment: fixed; background-size: cover;background-position: center center;"
+      );
+    }
+    const sp = () => {
+      for (let i = 0; i < paras.length; i++) {
+        let x = paras[i].getBoundingClientRect().top / modifier;
+        let y = Math.round(x * 100) / 100;
+        paras[i].style.backgroundPosition = "center " + y + "px";
+      }
+      requestAnimationFrame(sp);
+    };
+    requestAnimationFrame(sp);
+  };
+  
+  simpleParallax("para", 3);
+  
+
+
+// Smooth Scroll on mouse wheel
+function init(){
+    new SmoothScroll(document,120,12)
+}
+
+function SmoothScroll(target, speed, smooth) {
+	if (target === document)
+		target = (document.scrollingElement 
+              || document.documentElement 
+              || document.body.parentNode 
+              || document.body) // cross browser support for document scrolling
+      
+	var moving = false
+	var pos = target.scrollTop
+  var frame = target === document.body 
+              && document.documentElement 
+              ? document.documentElement 
+              : target // safari is the new IE
+  
+	target.addEventListener('mousewheel', scrolled, { passive: false })
+	target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
+
+	function scrolled(e) {
+		e.preventDefault(); // disable default scrolling
+
+		var delta = normalizeWheelDelta(e)
+
+		pos += -delta * speed
+		pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
+
+		if (!moving) update()
+	}
+
+	function normalizeWheelDelta(e){
+		if(e.detail){
+			if(e.wheelDelta)
+				return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1) // Opera
+			else
+				return -e.detail/3 // Firefox
+		}else
+			return e.wheelDelta/120 // IE,Safari,Chrome
+	}
+
+	function update() {
+		moving = true
+    
+		var delta = (pos - target.scrollTop) / smooth
+    
+		target.scrollTop += delta
+    
+		if (Math.abs(delta) > 0.5)
+			requestFrame(update)
+		else
+			moving = false
+	}
+
+	var requestFrame = function() { // requestAnimationFrame cross browser
+		return (
+			window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function(func) {
+				window.setTimeout(func, 1000 / 50);
+			}
+		);
+	}()
+}
+
+init()
